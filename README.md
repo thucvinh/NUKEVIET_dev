bcee8835d4b7d2cb48fe9af3751524601c8f00ca
Le Thuc Vinh <thucvinh_arc@yahoo.com.vn>
2016-04-12 17:28:24 +0700

    Xóa module conten

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 
 ##Tích hợp thêm một số module dùng sẵn
 
-###1. Module Home (tác giả: )
+1. Module Home (tác giả: Phan Tan Dung)
 
+2. Module slider (tác giả: Nhim Thu Lĩnh - đã fix lỗi)
 
+3. Module Hỗ trợ trực tuyến:
 
+4. Fix giao diện amdin
+
+5. Loại bỏ module freecontent
+
+6. Bổ sung Giao diện Dev
+
+7. Fix và loại bỏ một số block không dùng tới
 
 Website: http://tieplua.net | http://tieplua.com | http://tieplua.info | http://nguoikhuyettat.clup
 
--- modules/menu/blocks/global.site_mods.php
+++ modules/menu/blocks/global.site_mods.php
@@ -109,8 +109,7 @@ if (! nv_function_exists('nv_menu_site_mods')) {
                                 'link' => NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $modname . '&amp;' . NV_OP_VARIABLE . '=' . $l['alias']
                             );
                         }
-                    }
-                    if ($modvalues['module_file'] == 'shops') {
+                    }elseif ($modvalues['module_file'] == 'shops') {
                         $db->sqlreset()->select(NV_LANG_DATA . '_title as title, ' . NV_LANG_DATA . '_alias as alias')->from($db_config['prefix'] . '_' . $modvalues['module_data'] . '_catalogs')->where('parentid=0 AND inhome=1')->order('weight ASC')->limit(10);
                         $list = $nv_Cache->db($db->sql(), '', $modname);
                         foreach ($list as $l) {
