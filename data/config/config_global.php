c80579c73b2d9fc38e4b99b02abc9c81f48b789f
Le Thuc Vinh <thucvinh_arc@yahoo.com.vn>
2016-04-12 17:57:14 +0700

    .

--- data/config/config_global.php
+++ data/config/config_global.php
@@ -5,7 +5,7 @@
  * @Author VINADES.,JSC (contact@vinades.vn)
  * @Copyright (C) 2016 VINADES.,JSC. All rights reserved
  * @License GNU/GPL version 2 or any later version
- * @Createdate Tue, 12 Apr 2016 10:15:41 GMT
+ * @Createdate Tue, 12 Apr 2016 10:56:56 GMT
  */
 
 if ( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
@@ -105,7 +105,7 @@ $global_config['site_timezone']='byCountry';
 $global_config['spadmin_add_admin']=1;
 $global_config['ssl_https']=0;
 $global_config['str_referer_blocker']=0;
-$global_config['timestamp']=1;
+$global_config['timestamp']=2;
 $global_config['upload_alt_require']=1;
 $global_config['upload_auto_alt']=1;
 $global_config['upload_checking_mode']='strong';
